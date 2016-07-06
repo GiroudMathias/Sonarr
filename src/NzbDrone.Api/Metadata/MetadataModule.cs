ef01432b64a9acc79037b6445d837ca2f1b1bdb6
Taloth Saldono <Taloth@users.noreply.github.com>
2016-07-06 21:53:36 +0200

    Fixed Metadata Enable.

--- src/NzbDrone.Api/Metadata/MetadataModule.cs
+++ src/NzbDrone.Api/Metadata/MetadataModule.cs
@@ -13,11 +13,15 @@ namespace NzbDrone.Api.Metadata
         protected override void MapToResource(MetadataResource resource, MetadataDefinition definition)
         {
             base.MapToResource(resource, definition);
+
+            resource.Enable = definition.Enable;
         }
 
         protected override void MapToModel(MetadataDefinition definition, MetadataResource resource)
         {
             base.MapToModel(definition, resource);
+
+            definition.Enable = resource.Enable;
         }
 
         protected override void Validate(MetadataDefinition definition, bool includeWarnings)
